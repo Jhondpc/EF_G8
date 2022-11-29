79036b949492ef74a52d27093d1c41bdd8aa6f6a
william <william24jes@hotmail.com>
2022-11-29 12:38:09 -0500

    william - servlet sesion

--- EF_G8/src/main/java/com/example/ef_g8/beans/Usuarios.java
+++ EF_G8/src/main/java/com/example/ef_g8/beans/Usuarios.java
@@ -1,4 +1,49 @@
 package com.example.ef_g8.beans;
 
 public class Usuarios {
+    private int idUsuarios;
+    private String nombres;
+    private String apellidos;
+    private String correo;
+    private String tipo;
+
+    public int getIdUsuarios() {
+        return idUsuarios;
+    }
+
+    public void setIdUsuarios(int idUsuarios) {
+        this.idUsuarios = idUsuarios;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
